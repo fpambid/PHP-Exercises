b502404a8b81465ac983b50de822eb090693bf89
fpambid <f.pambid@klab.com>
2014-08-19 14:44:56 +0800

    added eol.php

---
+++ eol.php
@@ -0,0 +1,15 @@
+<?
+if (PHP_OS == "WIN32" || PHP_OS == "WINNT") {
+define('EOL', "\r\n");
+}
+else if (PHP_OS == "Linux") {
+define('EOL', "\n");
+}
+else {
+define('EOL', "\n");
+}
+function ln($out) {
+echo $out . EOL;
+}
+ln("this line will have the server platform's EOL character");
+?>
