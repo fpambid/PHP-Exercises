742bc2f6e6b01e160641e58ffd8d50eba398fd7b
fpambid <f.pambid@klab.com>
2014-08-19 13:51:02 +0800

    added security_color.php

---
+++ security_color.php
@@ -0,0 +1,11 @@
+<?
+<form action="process.php" method="POST">
+<p>Please select a color:
+<select name="color">
+<option value="red">red</option>
+<option value="green">green</option>
+<option value="blue">blue</option>
+</select>
+<input type="submit" /></p>
+</form>
+?>
