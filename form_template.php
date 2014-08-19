08fb4be176a9ccde81f22fd314109821e0037a83
fpambid <f.pambid@klab.com>
2014-08-19 14:10:21 +0800

    added form_template.php

---
+++ form_template.php
@@ -0,0 +1,12 @@
+<?php
+$bindings['DESTINATION'] = $_SERVER['PHP_SELF'];
+$name = $_GET['name'];
+if (!empty($name)) {
+// do something with the supplied values
+$template = "thankyou.template";
+$bindings['NAME'] = $name;
+}
+else {
+$template = "user.template";
+}
+echo fillTemplate($template, $bindings);
